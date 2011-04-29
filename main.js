00fb1e5027b79adb2585dc8d837d1b13c462efc5
Luca Matteis <lmatteis@gmail.com>
2011-04-29 15:50:58 +0200

    Added some UI

--- main.js
+++ main.js
@@ -9,6 +9,40 @@ apejs.urls = {
             response.getWriter().println(skin);
         }
     },
+    "/get-attribute": {
+        get: function(request, response) {
+            var term_id = request.getParameter("term_id");
+            if(!term_id) return response.getWriter().println("No term_id");
+
+            var q = googlestore.query("attribute");
+            q.queryFilter("term_id", "=", term_id);
+            var res = q.result();
+
+            var json = "[";
+            for(var i=0; i<res.length; i++) {
+                json += "{ \""+res[i].getKey().getName().replace("\"","\\\"") + "\":\""+res[i].getProperty("value").getValue().replace("\"","\\\"") + "\"} ";
+            }
+            json += "]";
+            response.getWriter().println(json);
+        }
+    },
+    "/add-attribute": {
+        post: function(request, response) {
+            function err(msg) { response.getWriter().println(msg); }
+
+            var key = request.getParameter("key"),
+                value = request.getParameter("value"),
+                term_id = request.getParameter("term_id");
+            if(key == "" || value == "" || term_id == "") return err("Must complete all fields");
+
+            var attribute = googlestore.entity("attribute", key, {
+                value: new Text(value),
+                term_id: term_id
+            });
+            googlestore.datastore.put(attribute);
+
+        }
+    },
     "/httpget": {
         get: function(request, response) {
             require("./httpget.js");
