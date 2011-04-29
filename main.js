4be9272a238a3a5f0b7b1df3f918fa18f34b14e4
Luca Matteis <lmatteis@gmail.com>
2011-04-29 10:10:37 +0200

    The JS in the UI talks to the OLS Api getting much better information.

--- main.js
+++ main.js
@@ -8,5 +8,14 @@ apejs.urls = {
             var skin = render("skins/index.html");
             response.getWriter().println(skin);
         }
+    },
+    "/httpget": {
+        get: function(request, response) {
+            require("./httpget.js");
+            var url = request.getParameter("url");
+            var ret = httpget(url);
+            response.setContentType("text/xml");
+            response.getWriter().println(ret);
+        }
     }
 };
