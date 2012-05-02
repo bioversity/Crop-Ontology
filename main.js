4ac588e1e545f5ab2f1b50f177e2d831356cbac4
Luca Matteis <lmatteis@gmail.com>
2012-05-02 16:29:12 +0200

    fixed and improved Report (print) functionality

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ var languages = require("./languages.js");
 // commonjs modules
 var Mustache = require("./common/mustache.js");
 
-var VERSION = "0.8.6";
+var VERSION = "0.8.8";
 var URL = 'http://www.cropontology.org';
 
 var isblank = function(javaStr) {
