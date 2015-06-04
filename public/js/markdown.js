dbe247ca09136d2ea61ee7bfe4de6f49c046f84b
marieALaporte <marie-angeliquelaporte@MacBook-Pro-de-Marie-Angelique.local>
2015-06-04 10:15:02 +0200

    Change TO link
    Now point to the planteome website

--- public/js/markdown.js
+++ public/js/markdown.js
@@ -22,7 +22,8 @@ function markdown(str) {
       if(prefix === "PO") {
         return "<a target='_blank' href='http://www.plantontology.org/amigo/go.cgi?view=details&search_constraint=terms&depth=0&query="+id+"'>" + id + "</a>"
       } else if(prefix === "TO") {
-        return "<a target='_blank' href='http://www.gramene.org/db/ontology/search?id="+id+"'>" + id + "</a>"
+        // old site : return "<a target='_blank' href='http://www.gramene.org/db/ontology/search?id="+id+"'>" + id + "</a>"
+        return "<a target='_blank' href='http://planteome.org/amigo/cgi-bin/crop_amigo/term_details?term="+id+"'>" + id + "</a>"
       } else {
         return id;
       }
