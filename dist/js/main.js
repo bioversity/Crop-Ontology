8f5447821856a8f84fa91f1228f61bf5509c3104
gubi <gubi.ale@iod.io>
2018-12-19 09:30:16 +0100

    Some enhancements

--- dist/js/main.js
+++ dist/js/main.js
@@ -69,6 +69,9 @@ module.exports={
     "contact-us": {
         "title": "Contact us"
     },
+    "feedback": {
+        "title": "Feedback"
+    },
     "add-ontology": {
         "title": "Add new terms"
     }
