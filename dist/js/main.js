8af65aaf6d1d5a7a6db56b430bf2f4cc649895fe
gubi <gubi.ale@iod.io>
2019-01-03 21:12:16 +0100

    This class is for future purposes

--- dist/js/main.js
+++ dist/js/main.js
@@ -6420,8 +6420,6 @@ Object.defineProperty(exports, "__esModule", {
     value: true
 });
 
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-
 var _navigation = require("../../src/es6/_navigation.es6");
 
 var _navigation2 = _interopRequireDefault(_navigation);
@@ -6433,55 +6431,9 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
 var NAV = new _navigation2.default(),
     page = NAV.get_page();
 
-var actions = function () {
-    function actions() {
-        _classCallCheck(this, actions);
-    }
-
-    _createClass(actions, [{
-        key: "execute",
-        value: function execute() {
-            if (page == "ontology" || page == "terms") {
-                this.edit_button_action();
-            }
-        }
-    }, {
-        key: "edit_button_action",
-        value: function edit_button_action() {
-            // Edit button action
-            $("#page_info .btn-mini").on("click", function (e) {
-                console.warn("mmm");
-                // let $dd = $(e.target).closest("dd"),
-                //     $dd2 = $dd.clone();
-                //
-                // $dd.html(
-                //     $('<form>', {"method": "post", "enctype": "multipart/form-data", "action": ""}).append(
-                //         $('<div>', {"class": "row"}).append(
-                //             $('<div>', {"class": "input-field col s12"}).append(
-                //                 $('<textarea>', {"class": "materialize-textarea", "name": "value"})
-                //             )
-                //         )
-                //     ).append(
-                //         $('<div>', {"class": "row"}).append(
-                //             $('<div>', {"class": "col s12"}).append(
-                //                 $('<a>', {"href": "javascript:;", "class": "grey-text left"}).text("‹ Cancel").on("click", () => {
-                //                     $dd.html($dd2.html());
-                //                 })
-                //             ).append(
-                //                 $('<a>', {"href": "javascript:;", "class": "btn btn-flat btn-highlight right"}).text("Save").on("click", () => {
-                //                     console.log("Okay!");
-                //                 })
-                //             )
-                //         )
-                //     )
-                // );
-                // console.log();
-            });
-        }
-    }]);
-
-    return actions;
-}();
+var actions = function actions() {
+    _classCallCheck(this, actions);
+};
 
 exports.default = actions;
 
--- src/es6/_actions.es6
+++ src/es6/_actions.es6
@@ -6,43 +6,7 @@ var NAV = new navigation(),
     page = NAV.get_page();
 
 class actions {
-    execute() {
-        if(page == "ontology" || page == "terms") {
-            this.edit_button_action();
-        }
-    }
 
-    edit_button_action() {
-        // Edit button action
-        $("#page_info .btn-mini").on("click", (e) => {
-            console.warn("mmm");
-            // let $dd = $(e.target).closest("dd"),
-            //     $dd2 = $dd.clone();
-            //
-            // $dd.html(
-            //     $('<form>', {"method": "post", "enctype": "multipart/form-data", "action": ""}).append(
-            //         $('<div>', {"class": "row"}).append(
-            //             $('<div>', {"class": "input-field col s12"}).append(
-            //                 $('<textarea>', {"class": "materialize-textarea", "name": "value"})
-            //             )
-            //         )
-            //     ).append(
-            //         $('<div>', {"class": "row"}).append(
-            //             $('<div>', {"class": "col s12"}).append(
-            //                 $('<a>', {"href": "javascript:;", "class": "grey-text left"}).text("‹ Cancel").on("click", () => {
-            //                     $dd.html($dd2.html());
-            //                 })
-            //             ).append(
-            //                 $('<a>', {"href": "javascript:;", "class": "btn btn-flat btn-highlight right"}).text("Save").on("click", () => {
-            //                     console.log("Okay!");
-            //                 })
-            //             )
-            //         )
-            //     )
-            // );
-            // console.log();
-        });
-    }
 }
 
 export default actions;
