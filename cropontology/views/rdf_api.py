9d98b3a527a202f8d4709e452eec325c7a543cb9
Marie-Angélique Laporte <marieangelique.laporte@gmail.com>
2021-06-28 16:01:43 +0200

    adding call for excel download

--- cropontology/views/rdf_api.py
+++ cropontology/views/rdf_api.py
@@ -7,6 +7,7 @@ import requests
 from rdflib import Graph
 from rdflib.namespace import OWL, DC, DCTERMS, RDF, RDFS, SKOS
 from rdflib import URIRef, BNode, Literal
+import pandas
 
 
 def to_json(data):
@@ -19,18 +20,25 @@ def get_neo_result(cursor, key):
         results.append(an_item[key])
     return results
 
+def get_neo_result_2keys(cursor, key, key2):
+    results = {}
+    for an_item in cursor:
+        results[key]=an_item[key]
+        results[key2]=an_item[key2]
+
+    return results
 
 def get_trait(db, term_id):
     query = (
         'Match (variable {id:"'
         + term_id
-        + '"}) -[VARIABLE_OF]->(trait {term_type: "trait"}) return trait'
+        + '"}) -[VARIABLE_OF]->(trait {term_type: "trait"}) return an_item["trait"]'
     )
     cursor = db.run(query)
     trait = get_neo_result(cursor, "trait")
-    if trait:
+    if an_item["trait"]:
         trait = trait[0]
-    return trait
+    return an_item["trait"]
 
 
 def get_method(db, term_id):
@@ -50,16 +58,16 @@ def get_scale(db, term_id):
     query = (
         'Match (variable {id:"'
         + term_id
-        + '"}) -[VARIABLE_OF]->(scale {term_type: "scale"}) return scale'
+        + '"}) -[VARIABLE_OF]->(scale {term_type: "scale"}) return an_item["scale"]'
     )
     cursor = db.run(query)
-    scale = get_neo_result(cursor, "scale")
-    if scale:
-        scale = scale[0]
-    return scale
+    an_item["scale"] = get_neo_result(cursor, "scale")
+    if an_item["scale"]:
+        an_item["scale"] = an_item["scale"][0]
+    return an_item["scale"]
 
 
-class EBIMetadataView(PublicView):
+class MetadataView(PublicView):
     def process_view(self):
         self.returnRawViewResult = True
         headers = [
@@ -84,13 +92,13 @@ class EBIMetadataView(PublicView):
                     ret += (
                         " - id: " + onto_id + "\n"
                         "   title: " + onto_name + " ontology\n"
-                        "   uri: http://www.cropontology.org/ontology/"
+                        "   uri: http://127.0.0.1:5900/ontology/"
                         + onto_id
                         + "/"
                         + onto_name
                         + "\n"
                         '   description: "' + onto_description + '"' + "\n"
-                        "   homepage: http://www.cropontology.org/ontology/"
+                        "   homepage: http://127.0.0.1:5900/ontology/"
                         + onto_id
                         + "/"
                         + onto_name
@@ -106,7 +114,7 @@ class EBIMetadataView(PublicView):
                         "     - http://www.cropontology.org/rdf/scale_of\n"
                         "   base_uri:\n"
                         "     - http://www.cropontology.org/rdf/" + onto_id + "\n"
-                        "   ontology_purl : http://www.cropontology.org/ontology/"
+                        "   ontology_purl : http://127.0.0.1:5900/ontology/"
                         + onto_id
                         + "/"
                         + onto_name
@@ -200,7 +208,7 @@ class RDFCleanView(PublicView):
             if "scientist" in var:
                 g.add((var_uri, DCTERMS.contributor, Literal(var["scientist"])))
 
-            ##add trait information
+            ##add an_item["trait"] information
             trait = get_trait(db, var["id"])
             trait_uri = URIRef(NS + trait["id"])
             g.add((trait_uri, RDF.type, OWL.Class))
@@ -212,10 +220,10 @@ class RDFCleanView(PublicView):
                     Literal(trait["trait_description"], lang="en"),
                 )
             )
-            if "trait_synonym" in trait:
-                for syn in trait["trait_synonym"].split(","):
+            if "trait_synonym" in an_item["trait"]:
+                for syn in an_item["trait"]["trait_synonym"].split(","):
                     g.add((trait_uri, SKOS.altLabel, Literal(syn, lang="en")))
-            if "main_trait_abbreviation" in trait:
+            if "main_trait_abbreviation" in an_item["trait"]:
                 g.add(
                     (
                         trait_uri,
@@ -223,14 +231,14 @@ class RDFCleanView(PublicView):
                         Literal(trait["main_trait_abbreviation"], lang="en"),
                     )
                 )
-            if "alternative_abbreviation" in trait:
-                for syn in trait["alternative_abbreviation"].split(","):
+            if "alternative_abbreviation" in an_item["trait"]:
+                for syn in an_item["trait"]["alternative_abbreviation"].split(","):
                     g.add((trait_uri, SKOS.altLabel, Literal(syn, lang="en")))
-            if "entity" in trait:
+            if "entity" in an_item["trait"]:
                 g.add((trait_uri, entity, Literal(trait["entity"])))
-            if "attribute" in trait:
+            if "attribute" in an_item["trait"]:
                 g.add((trait_uri, attribute, Literal(trait["attribute"])))
-            if "xref" in trait:
+            if "xref" in an_item["trait"]:
                 g.add((trait_uri, DCTERMS.source, Literal(trait["trait_xref"])))
             g.add(
                 (
@@ -293,12 +301,12 @@ class RDFCleanView(PublicView):
             g.add((br, OWL.someValuesFrom, trait_uri))
             g.add((method_uri, RDFS.subClassOf, br))
 
-            ## add scale information
-            scale = get_scale(db, var["id"])
-            scale_uri = URIRef(NS + scale["id"])
+            ## add an_item["scale"] information
+            an_item["scale"] = get_scale(db, var["id"])
+            scale_uri = URIRef(NS + an_item["scale"]["id"])
             g.add((scale_uri, RDF.type, OWL.Class))
             g.add((scale_uri, RDFS.label, Literal(scale["name"], lang="en")))
-            if "xref" in scale:
+            if "xref" in an_item["scale"]:
                 g.add((scale_uri, DCTERMS.source, Literal(scale["scale_xref"])))
             g.add(
                 (
@@ -309,7 +317,7 @@ class RDFCleanView(PublicView):
             )
             g.add(
                 (
-                    URIRef(NS + scale["scale_class"].replace(" ", "_")),
+                    URIRef(NS + scale["scale"]["scale_class"].replace(" ", "_")),
                     RDFS.subClassOf,
                     URIRef(NS + "Scale"),
                 )
@@ -365,5 +373,84 @@ class RDFCleanView(PublicView):
             g.add((scale_uri, RDFS.subClassOf, br))
 
         response.text = g.serialize(format="pretty-xml").decode("utf-8")
+        db.close()
+        return response
+
+class ExcelView(PublicView):
+    def process_view(self):
+        self.returnRawViewResult = True
+        ontology_id = self.request.matchdict["ontology_id"]
+
+        headers = [
+            #("Content-Type", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet; charset=utf-8"), 
+            ("Content-Type", "text/csv; charset=utf-8"), ## csv
+        ]
+        response = Response(headerlist=headers, status=200)
+
+        neo4j_bolt_url = self.request.registry.settings["neo4j.bolt.ulr"]
+        neo4j_user = self.request.registry.settings["neo4j.user"]
+        neo4j_password = self.request.registry.settings["neo4j.password"]
+        driver = GraphDatabase.driver(neo4j_bolt_url, auth=(neo4j_user, neo4j_password))
+        db = driver.session()
+
+        query = ('match (variable:Variable{ontology_id:"'+ontology_id+'"})-[:VARIABLE_OF]->(trait:Trait) ' + 
+                    'match (variable:Variable{ontology_id:"CO_339"})-[:VARIABLE_OF]->(method:Method) ' +
+                    'match (variable:Variable{ontology_id:"CO_339"})-[:VARIABLE_OF]->(scale:Scale) ' +
+                    'return variable, trait, method, an_item["scale"]')
+        cursor = db.run(query)
+        
+        #terms = get_neo_result_4keys(cursor, "variable", "trait", "method")
+
+        results = []
+
+        for an_item in cursor:
+            ret_result = {
+                        "curation": "",
+                        "Variable ID": an_item["variable"]["id"],
+                        "Variable name": an_item["variable"]["name"],
+                        "Variable synonyms": an_item["variable"]["variable_synonyms"],
+                        "Context of use": an_item["variable"]["context_of_use"],
+                        "Growth stage": an_item["variable"]["growth_stage"],
+                        "Variable status": an_item["variable"]["variable_status"],
+                        "Variable Xref": an_item["variable"]["variable_xref"],
+                        "Institution": an_item["variable"]["institution"],
+                        "Scientist": an_item["variable"]["scientist"],
+                        "Date": an_item["variable"]["date"],
+                        "Language": an_item["variable"]["language"],
+                        "Crop": an_item["variable"]["crop"],
+                        "Trait ID": trait["id"],
+                        "Trait name": trait["name"],
+                        "Trait class": trait["trait_class"],
+                        "Trait description": trait["trait_description"],
+                        "Trait synonyms": trait["trait_synonym"], 
+                        "Main trait abbreviation": trait["main_trait_abbreviation"],
+                        "Alternative trait abbreviations": trait["alternative_abbreviation"],
+                        "Entity": trait["entity"],
+                        "Attribute": trait["attribute"],
+                        "Trait status": trait["trait_status"],
+                        "Trait Xref": trait["trait_xref"],
+                        "Method ID": an_item["method"]["id"],
+                        "Method name": an_item["method"]["name"],
+                        "Method class": an_item["method"]["method_class"],
+                        "Method description": an_item["method"]["method_description"],
+                        "Formula": an_item["method"]["formula"],
+                        "Method reference": an_item["method"]["method_reference"],
+                        "Scale ID": an_item["scale"]["id"],
+                        "Scale name": an_item["scale"]["name"],
+                        "Scale class": an_item["scale"]["scale_class"],
+                        "Decimal places": an_item["scale"]["decimal_places"],
+                        "Lower limit": an_item["scale"]["lower_limit"],
+                        "Upper limit": an_item["scale"]["upper limit"],
+                        "Scale Xref": an_item["scale"]["scale_xref"],
+                }
+
+            
+            print(ret_result)
+            results.append(ret_result)
+            
+            break
+
+        
+        response.text = to_json(results)
         db.close()
         return response
